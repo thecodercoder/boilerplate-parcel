0b872df3107c103aae734937284d5fa5e2024659
Jessica <>
2019-01-25 23:31:15 -0600

    initial files

---
+++ script.js
@@ -0,0 +1,6 @@
+import '/style.scss';
+
+console.log("hello world");
+
+const testNumber = 12345;
+console.log(testNumber);
---
+++ script2.js
@@ -0,0 +1,5 @@
+function test(){
+    console.log("this is a test");
+}
+
+test();
